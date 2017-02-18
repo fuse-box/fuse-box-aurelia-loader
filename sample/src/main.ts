cb75d3b04914dfa69fead4eaddfb8bec81ce111d
Daniel Bendel <daniel.bendel@gmail.com>
2017-02-18 10:15:48 +0100

    fix some TS errors

--- sample/src/main.ts
+++ sample/src/main.ts
@@ -4,12 +4,12 @@
 // debugger // enable to see issue
 // FuseBox.packages["fuse-box-aurelia-loader"] <- missing entry
 // FuseBox.packages["aurelia-v-grid"] <- missing entry
-FuseBox.pkg("fuse-box-aurelia-loader", {}, function(___scope___) {
+FuseBox.pkg("fuse-box-aurelia-loader", {}, function(___scope___: any) {
         ___scope___.entry = "fuse-box-aurelia-loader.js"
 });
 
-FuseBox.pkg("aurelia-v-grid", {}, function(___scope___) {
-        ___scope___.entry = "index.js"
+FuseBox.pkg("aurelia-v-grid", {}, function(___scope___: any) {
+        ___scope___.entry = 'index.js'
 });
 
 
@@ -18,21 +18,22 @@ FuseBox.pkg("aurelia-v-grid", {}, function(___scope___) {
 
 
 // add custom loader for fuse
-window.FUSEBOX_AURELIA_LOADER_LOGGING=true
+// window.FUSEBOX_AURELIA_LOADER_LOGGING=true
 import 'fuse-box-aurelia-loader'
 
 
 // start aurelia bootstrapper
 import 'aurelia-bootstrapper';
+import {Aurelia} from 'aurelia-framework';
 
 
 
 // aurelia configuration
-export function configure(aurelia) {
+export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-    .plugin("aurelia-v-grid")
+    .plugin('aurelia-v-grid')
 
   aurelia.start().then(() => aurelia.setRoot());
 }
