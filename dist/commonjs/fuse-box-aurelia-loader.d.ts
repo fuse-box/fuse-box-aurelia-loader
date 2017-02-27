b974d198c5143aeb34cf23566278c3da583e190c
Vegar Ringdal <vegar.ringdal@gmail.com>
2017-02-27 20:37:28 +0100

    chore(all) new build

--- dist/commonjs/fuse-box-aurelia-loader.d.ts
+++ dist/commonjs/fuse-box-aurelia-loader.d.ts
@@ -1,20 +1,28 @@
-import { Loader } from 'aurelia-loader';
+import { Loader, TemplateRegistryEntry } from 'aurelia-loader';
+export declare type LoaderPlugin = {
+    fetch: (address: string) => Promise<TemplateRegistryEntry> | TemplateRegistryEntry;
+};
+export declare class TextTemplateLoader {
+    loadTemplate(loader: Loader, entry: TemplateRegistryEntry): Promise<void>;
+}
+export declare function ensureOriginOnExports(moduleExports: any, moduleId: string): any;
 export declare class FuseBoxAureliaLoader extends Loader {
     textPluginName: string;
     loaderPlugins: any;
     moduleRegistry: any;
     templateRegistry: any;
-    templateLoader: any;
+    templateLoader: TextTemplateLoader;
+    modulesBeingLoaded: Map<string, Promise<any>>;
     constructor();
-    useTemplateLoader(templateLoader: any): void;
+    useTemplateLoader(templateLoader: TextTemplateLoader): void;
     loadAllModules(ids: any): Promise<any>;
     loadTemplate(url: any): Promise<any>;
     loadText(url: any): Promise<any>;
-    loadModule(id: any): Promise<any>;
+    loadModule(moduleId: any): Promise<any>;
     addPlugin(pluginName: any, implementation: any): void;
     normalize(moduleId: any, relativeTo: any): Promise<any>;
     map(id: any, source: any): void;
-    _import(address: any): any;
+    _import(address: any): Promise<any>;
     applyPluginToUrl(url: any, pluginName: any): string;
     loadWithFusebox(args: any): any;
     fuseBoxExist(id: any): boolean;
