b2ba3f7b35dabe1a79ef0bfdca62686292f62965
Vegar ringdal <vegar.ringdal@gmail.com>
2017-06-27 19:57:03 +0200

    chore(all): new build

--- dist/commonjs/fuse-box-aurelia-hmr-plugin.d.ts
+++
@@ -1,9 +0,0 @@
-import { FuseBoxAureliaLoader } from './fuse-box-aurelia-loader';
-export declare class FuseBoxAureliaHmrPlugin {
-    private context;
-    private reloadPageOnly;
-    private timer;
-    private loader;
-    constructor(loader: FuseBoxAureliaLoader, reloadPageOnly: boolean);
-    hmrUpdate(data: any): boolean;
-}
